60c956f853a46f0ffd2504f6b9e405f7b84a4395
jagapher <48227538+jagapher@users.noreply.github.com>
2020-04-18 14:55:13 -0400

    set up planting

--- scripts/Field.gd
+++ scripts/Field.gd
@@ -1,16 +1,24 @@
 extends Node
+
+#SIGNALS
+signal field_start_growing
+signal field_stop_growing
+
+#PREFABS
 var SeedPrefab = preload("res://scenes/Seed.tscn") 
+
+#EXPORTS
+export(bool) var plantable = true
 export (NodePath) var Inventory 
 export (NodePath) var MainScene 
-var InventoryPos
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
+#MEMBERS
+var InventoryNode
+var planted_attrs : PlantAttributes = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	InventoryPos = get_node(Inventory).position
+	InventoryNode = get_node(Inventory)
 	pass # Replace with function body.
 
 
@@ -18,15 +26,27 @@ func _ready():
 #func _process(delta):
 #	pass
 
-func plant(attributes):
+func plant(attributes : PlantAttributes):
 	#generate a new seed with mutated attributes
+	if (planted_attrs == null):
+		planted_attrs = attributes.duplicate()
+		emit_signal("field_start_growing", $Timer)
+		$Timer.start(planted_attrs.growth_time)
+	
+	print(planted_attrs.mutation_rate)
+		
 	var new_seed = SeedPrefab.instance()
 	get_node(MainScene).add_child(new_seed)
 	var attr = new_seed.get_node("PlantAttributes")
 	attr.replace_by(attributes.duplicate())
 	attr = new_seed.get_node("PlantAttributes")
 	attr.mutate()
-	new_seed.position = InventoryPos
+	new_seed.position = InventoryNode.position
 	print(new_seed.get_node("PlantAttributes").mutation_rate)
 	#move the seed to inventory
 	#makeseed(attributes.Type, attributes.mutation_rate, ....)
+
+
+func _on_Timer_timeout():
+	# add 4 plants to inventory after mutation
+	pass
