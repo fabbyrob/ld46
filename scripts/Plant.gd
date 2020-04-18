1704d044998336d3294a1c9ba4ff9bdcd24c7466
jagapher <48227538+jagapher@users.noreply.github.com>
2020-04-17 23:24:20 -0400

    generic plant/seed behaviors and scenes added

---
+++ scripts/Plant.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var SeedPreload = preload("res://scenes//JamsSeed.tscn")
+
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	rng.randomize()
+
+func get_seed(clone = false):
+	#create a new blank seed instance
+	var _seed = SeedPreload.instance()
+	
+	#replace the default attributes with a copy of the parent attributes
+	_seed.get_node("Attributes").replace_by($Attributes.duplicate())
+	
+	#optionally mutate
+	if !clone && rng.randf() < $Attributes.mutation_rate:
+		_seed.get_node("Attributes").mutate()
+	
+	
+	return _seed 
---
+++ scripts/PlantAttributes.gd
@@ -0,0 +1,16 @@
+extends Object
+
+#TYPES
+enum PlantType {Potato, Cucumber, Turnip}
+
+#EXPORTS
+export(PlantType) var plant_type = PlantType.Potato
+
+export(float,0,1,0.05) var mutation_rate = 0.1
+
+export(int,0,100,5) var food_amount = 15
+
+export(float,0,30,0.5) var growth_time = 10
+
+func mutate():
+	pass
