7b3be091d7509933f825ff219e9d942977a147ee
jagapher <48227538+jagapher@users.noreply.github.com>
2020-04-19 12:59:06 -0400

    working on field attributes

--- scripts/Field.gd
+++ scripts/Field.gd
@@ -6,24 +6,25 @@ class_name Field
 var SeedScene = load("res://scenes/Items/Seed.tscn")
 
 #EXPORTS
+export(Fakeenum.NamedEnum) var x #= Fakeenum.NamedEnum.ONE
 export(bool) var plantable = true
 export var mutation_modifier = 1.0
 
 #MEMBERS
-var planted_attrs : PlantAttributes = null
+var field_attrs = FieldAttributes.new()
+var planted_attrs = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+#METHODS
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func plant(attributes : PlantAttributes):
+func plant(attributes):
 	#generate a new seed with mutated attributes
-	if (plantable && is_empty()):
+	if (plantable):
+		#delete any existing plants
+		for i in range(1, 5):
+			var plot = get_node("plot%d" % [i])
+			if (plot.get_item()):
+				plot.get_item().queue_free()
+				plot.set_item(null)
 		#store the planted attributes
 		planted_attrs = attributes.duplicate()
 		#start the growth timer
@@ -53,7 +54,7 @@ func _on_Timer_timeout():
 		var new_plant = SeedScene.instance()
 		new_plant.get_node("PlantAttributes").replace_by(grown_attrs)
 		get_tree().get_root().get_node("MainScene").add_child(new_plant)
-		grown_attrs.mutate(mutation_modifier)
+		grown_attrs.mutate(field_attrs)
 		new_plant.update_self() 
 		#add plant item to scene in one of the field slots
 		var plot = get_node("plot%d" % [i+1]) as ItemSlot
