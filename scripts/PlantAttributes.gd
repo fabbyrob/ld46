da74049463c5022010c27ef91f089269587d4b2a
jagapher <48227538+jagapher@users.noreply.github.com>
2020-04-18 20:41:32 -0400

    actually use some randomizer stuff

--- scripts/PlantAttributes.gd
+++ scripts/PlantAttributes.gd
@@ -15,6 +15,8 @@ export(int,0,100,5) var food_amount = 15
 export(float,0,30,0.5) var growth_time = 5
 
 func mutate():
-	mutation_rate += Randomizer.sample()
+	if Randomizer.sample() < mutation_rate:
+		mutation_rate += Randomizer.sample()
+		
 	print("post mutation %s" % mutation_rate)
 
